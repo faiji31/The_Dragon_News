85c03a761b6e7791ad4a2c6d993124edfda7859b
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-28 17:28:07 +0600

    5th commit

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { use } from 'react';
+
+
+const CategoryPromise = fetch("/public/categories.json").then((res)=>res.json());
 
 const Categories = () => {
+    const categories = use(CategoryPromise)
       return (
             <div>
-                  <h2 className='font-bold'>All categories</h2>
+                  <h2 className='font-bold'>All categories({categories.length})</h2>
             </div>
       );
 };
--- src/components/homeLayout/LeftAside.jsx
+++ src/components/homeLayout/LeftAside.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import Categories from '../Categories';
+import { Suspense } from 'react';
 
 const LeftAside = () => {
       return (
             <div>
-                  <Categories></Categories>
+                 <Suspense fallback={<span className="loading loading-dots loading-xl"></span>}>
+                   <Categories></Categories>
+                 </Suspense>
             </div>
       );
 };
