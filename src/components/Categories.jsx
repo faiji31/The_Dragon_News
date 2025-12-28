eb5cd89ea21e56a871ce4ac182d179a5f1581e13
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-28 19:03:28 +0600

    7th commit

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -7,10 +7,11 @@ const CategoryPromise = fetch("/public/categories.json").then((res)=>res.json())
 const Categories = () => {
     const categories = use(CategoryPromise)
       return (
-            <div className='grid grid-cols-1 mt-5'>
+            <div className='grid grid-cols-1 mt-5 gap-3'>
                   <h2 className='font-bold'>All categories({categories.length})</h2>
                   {
-                        categories.map(category=><NavLink key={category.id} className={"btn"}>{category.name}</NavLink>)
+                        categories.map(category=><NavLink key={category.id} className={"btn bg-base-100 border-0 hover:bg-base-200 font-semibold text-accent"} to={`/category/${category.id}`}
+                        >{category.name}</NavLink>)
                   }
             </div>
       );
