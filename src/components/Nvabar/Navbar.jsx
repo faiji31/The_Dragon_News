9e676af78f1de259546fec761ce53a4beb4549b3
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-28 10:25:07 +0600

    2nd commit

--- src/components/Nvabar/Navbar.jsx
+++ src/components/Nvabar/Navbar.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import React from "react";
+import { NavLink } from "react-router";
+import User from "../../assets/user.png"
 
 const Navbar = () => {
-      return (
-            <div>
-                  This is Navbar
-            </div>
-      );
+  return (
+
+    <div className="flex justify-between items-center">
+      <div className=""></div>
+      <div className="flex gap-5">
+        <NavLink to="/">Home</NavLink>
+        <NavLink to="/about">about</NavLink>
+        <NavLink to="/carrer">Carrer</NavLink>
+      </div>
+      <div className="flex gap-5">
+            <img src={User} alt="" />
+            <button className="btn btn-primary px-10">Login</button>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
