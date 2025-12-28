5cfd13b41d381292333e31534125ed97b6322cb6
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-28 22:52:05 +0600

    17th commit

--- src/Pages/Login.jsx
+++ src/Pages/Login.jsx
@@ -1,22 +1,29 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Login = () => {
-      return (
-            <div className='flex justify-center min-h-screen items-center'>
-                 <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <div className="card-body">
-        <fieldset className="fieldset">
-          <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
-          <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
-          <div><a className="link link-hover">Forgot password?</a></div>
-          <button className="btn btn-neutral mt-4">Login</button>
-        </fieldset>
+  return (
+    <div className="flex justify-center min-h-screen items-center">
+      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+            <h2 className="font-bold text-center text-2xl">Login Your Account</h2>
+        <div className="card-body">
+          <fieldset className="fieldset">
+            <label className="label">Email</label>
+            <input type="email" className="input" placeholder="Email" />
+            <label className="label">Password</label>
+            <input type="password" className="input" placeholder="Password" />
+            <div>
+              <a className="link link-hover">Forgot password?</a>
+            </div>
+           
+            <button className="btn btn-neutral mt-4">Login</button>
+             <p className="font-semibold text-center pt-5">Don't have an Account? <Link className="text-secondary"
+              to="/auth/Register">Register</Link></p>
+          </fieldset>
+        </div>
       </div>
     </div>
-            </div>
-      );
+  );
 };
 
 export default Login;
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router";
 
 const Register = () => {
-      return (
-            <div>
-                  This is Register
-            </div>
-      );
+  return (
+    <div>
+      <div className="flex justify-center min-h-screen items-center">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl py-5">
+          <h2 className="font-bold text-center text-2xl">Register Your Account</h2>
+          <div className="card-body">
+            <fieldset className="fieldset">
+              <label className="label">Email</label>
+              <input type="email" className="input" placeholder="Email" />
+              <label className="label">Password</label>
+              <input type="password" className="input" placeholder="Password" />
+              <div>
+                <a className="link link-hover">Forgot password?</a>
+              </div>
+
+              <button className="btn btn-neutral mt-4">Login</button>
+              <p className="font-semibold text-center pt-5">
+                Don't have an Account?{" "}
+                <Link className="text-secondary" to="/auth/Register">
+                  Register
+                </Link>
+              </p>
+            </fieldset>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Register;
