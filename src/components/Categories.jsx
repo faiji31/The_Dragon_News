635a7515eaaad826b6b9eb4653f54ee72c6f60ad
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-28 18:47:45 +0600

    6th commit

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import { NavLink } from 'react-router';
 
 
 const CategoryPromise = fetch("/public/categories.json").then((res)=>res.json());
@@ -6,8 +7,11 @@ const CategoryPromise = fetch("/public/categories.json").then((res)=>res.json())
 const Categories = () => {
     const categories = use(CategoryPromise)
       return (
-            <div>
+            <div className='grid grid-cols-1 mt-5'>
                   <h2 className='font-bold'>All categories({categories.length})</h2>
+                  {
+                        categories.map(category=><NavLink key={category.id} className={"btn"}>{category.name}</NavLink>)
+                  }
             </div>
       );
 };
