54e9c713b74e98aefd8f09332f94dfebdfa97f6a
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-28 19:48:37 +0600

    10th commit

--- src/Pages/CategoryNews.jsx
+++ src/Pages/CategoryNews.jsx
@@ -9,11 +9,23 @@ const CategoryNews = () => {
       const [categoryNews,setCategoryNews]=useState([]);
 
       useEffect(()=>{
-            const filterNews = data.filter(news=>news.category_id==id);
+            if(id== "0"){
+                  // eslint-disable-next-line react-hooks/set-state-in-effect
+                  setCategoryNews(data)
+            }
+            else if(id =="1"){
+                  const filterNews = data.filter(news=>news.others.is_today_pick== true);
+                  setCategoryNews(filterNews);
+            
+            }
+            else{
+                 const filterNews = data.filter(news=>news.category_id==id);
             console.log(filterNews);
 
             // eslint-disable-next-line react-hooks/set-state-in-effect
             setCategoryNews(filterNews);
+            }
+           
       },[data,id])
       return (
             <div>
