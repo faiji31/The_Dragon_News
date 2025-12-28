51ceae9b65d47e4eaab8a6480da8457a283618a1
Md.Faiji Akbar <faijiakbarliam2016@gmail.com>
2025-12-28 22:58:19 +0600

    18th commit

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -9,19 +9,23 @@ const Register = () => {
           <h2 className="font-bold text-center text-2xl">Register Your Account</h2>
           <div className="card-body">
             <fieldset className="fieldset">
+              <label className="label">Name</label>
+              
+              <input type="text" className="input" placeholder="Name" />
+              <label className="label">Photo URL</label>
+              
+              <input type="text" className="input" placeholder="Photo URL" />
               <label className="label">Email</label>
               <input type="email" className="input" placeholder="Email" />
               <label className="label">Password</label>
               <input type="password" className="input" placeholder="Password" />
-              <div>
-                <a className="link link-hover">Forgot password?</a>
-              </div>
+              
 
-              <button className="btn btn-neutral mt-4">Login</button>
+              <button className="btn btn-neutral mt-4">Register</button>
               <p className="font-semibold text-center pt-5">
-                Don't have an Account?{" "}
-                <Link className="text-secondary" to="/auth/Register">
-                  Register
+                Already have an Account?
+                 <Link className="text-secondary" to="/auth/Login">
+                  Login
                 </Link>
               </p>
             </fieldset>
